ae91f5aa55c9e95ed889639f648f3edada18899e
Denzel C. Mathew <dmathew@lin116-02.cise.ufl.edu>
2016-03-07 17:49:32 -0500

    fuck shit up

--- Project2/part2.c
+++ Project2/part2.c
@@ -1,20 +1,56 @@
 #include <stdio.h>
 
+#define M 4
+#define N 4
+
+
 int main(void){
 
 	//row and column
-	int row = 3;
-	int col = 3;
-	int endingRow = row - 1;
-	int endingCol = col - 1;
-
+	int i;
+	int left = 0, right = N-1, top = 0, bottom = M-1;
 
 	//initialize array
-	int array[row][col] = {{1,2,3},{4,5,6},{7,8,9}};
+	int array[M][N] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 
-	//loop through to find top row
-	for(int i = 0; i < row; i++){
-		printf("%d\n", array[i][col]);
+	printf("\n");
+
+	while(left <= right || top <= bottom){
+		//from left to right print top
+		for(i = left; i <= right; i++){
+			printf("%d,", array[top][i]);
+		}
+
+		//increment top 
+		top++;
+
+
+		//from top to bottom print right
+		for(i = top; i <= bottom; i++){
+			printf("%d,", array[i][right]);
+		}
+
+		//decrement right;
+		right--;
+		
+		//from right to left print bottom
+		for(i = right; i >= left; i--){
+			printf("%d,", array[bottom][i]);
+		}
+
+		//increment bottom
+		bottom--;
+
+		//from bottom to top, print left
+		for(i = bottom; i >= top; i--){
+			printf("%d,", array[i][left]);
+		}
+
+		//increment left
+		left++;
+
+
 	}
-
+			printf("\n");
 }
+ 
